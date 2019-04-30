64be129a508f1f35bd69875cd0a518daa9a7f5c5
jerin john <john.jerin2@gmail.com>
2019-04-30 09:24:31 +0530

    report viewer added

---
+++ RDLC_Reports_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RDLC_Reports_MVC.MvcApplication" Language="C#" %>
---
+++ RDLC_Reports_MVC/Reports/Report.aspx.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Reporting.WebForms;
+using RDLC_Reports_MVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RDLC_Reports_MVC.Reports
+{
+    public partial class Report : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("Report.rdlc");
+                NorthwindEntities entities = new NorthwindEntities();
+                ReportDataSource datasource = new ReportDataSource("Customers", (from customer in entities.Customers.Take(10)
+                                                                                 select customer));
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(datasource);
+            }
+        }
+    }
+}
